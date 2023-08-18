072e514a19c7b17324e33a0ba42aebb519fd1ca0
murat4488 <muratguney@msn.com>
2023-08-18 09:54:53 -0400

    Added React Testing Exercises

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Carousel from "./Carousel";
+import Card from "./Card";
+import photos from "./photos.js";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //const linkElement = screen.getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+});
+
+test("renders each components work properly", () => {
+  render(<Card />);
+  render(<Carousel photos={photos} title="" />);
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<Carousel photos={photos} title="" />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<Card />);
+  expect(asFragment()).toMatchSnapshot();
 });
---
+++ src/Carousel.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "./Carousel.css";
+import Card from "./Card";
+
+/** Carousel: displays images and arrows to navigate through them
+ *
+ * Props:
+ * - photos: array of {src, caption} objects
+ * - title: string describing the collection of images
+ *
+ * State:
+ * - currCardIdx: integer for current card index
+ *
+ * App --> Carousel --> Card
+ */
+function Carousel({ photos, title }) {
+  const [currCardIdx, setCurrCardIdx] = useState(0);
+
+  const currCard = photos[currCardIdx];
+  const total = photos.length;
+
+  //Increments currCardIdx state by 1
+  function goForward() {
+    setCurrCardIdx(currCardIdx + 1);
+  }
+
+  //Decrements currCardIdx state by 1
+  function goBackward() {
+    setCurrCardIdx(currCardIdx - 1);
+  }
+
+  const hideLeftArrow =
+    currCardIdx > 0
+      ? "bi bi-arrow-left-circle"
+      : "bi bi-arrow-left-circle disabled";
+  const hideRightArrow =
+    currCardIdx < photos.length - 1
+      ? "bi bi-arrow-right-circle"
+      : "bi bi-arrow-right-circle disabled";
+
+  return (
+    <div className="Carousel">
+      <h1>{title}</h1>
+      <div className="Carousel-main">
+        <i className={hideLeftArrow} onClick={goBackward} />
+
+        <Card
+          caption={currCard.caption}
+          src={currCard.src}
+          currNum={currCardIdx + 1}
+          totalNum={total}
+        />
+        <i className={hideRightArrow} onClick={goForward} />
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
